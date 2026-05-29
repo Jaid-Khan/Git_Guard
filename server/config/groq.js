1df0afdad835a59640539854741a37e5ed6c6c8d
JAID-KHAN <jaidkhan04072001@gmail.com>
2026-05-29 21:45:06 +0530

    Integrated Groq LLM for automated AI pull request code reviews

--- server/config/groq.js
+++ server/config/groq.js
@@ -5,3 +5,13 @@ const groq = new Groq({
 });
 
 module.exports = groq;
+
+// const Groq = require("groq-sdk");
+
+// console.log("GROQ KEY:", process.env.GROQ_API_KEY);
+
+// const groq = new Groq({
+//   apiKey: process.env.GROQ_API_KEY,
+// });
+
+// module.exports = groq;
